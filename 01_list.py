96542719100b1d86e8925aeeb3eda8613b7c703f
Aman kumar <140222816+amankumar1466@users.noreply.github.com>
2023-08-07 18:58:06 +0530

    Add files via upload

---
+++ 01_list.py
@@ -0,0 +1,11 @@
+a=[1, 2, 3, 4, 5, 6]
+#print the list using print() function 
+#print(a)
+#access using index using a[0], a[1],a[2]
+#print(a[2])
+# change the value of list using
+a[0]=98
+print(a)
+# we can create a list with item of different types
+c= [45, "aman", False, 6.9]
+print(c)
